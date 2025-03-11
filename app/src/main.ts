422edd3fad00ac17b967bcd21dad2b303e9a7adb
benjamin-kunai <benjamin.hybart@kunaico.com>
2025-03-11 16:22:40 -0400

    Experiment with some basic api calls

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,24 +1,15 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { setUpListeners } from './api';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Vite/TypeScript App</h1>
+    <input type="text"/>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button  type="button">Fetch Some Data</button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setUpListeners();
