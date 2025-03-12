b4808e6fd5e97a92ccc12a71642099f11bdd3452
benjamin-kunai <benjamin.hybart@kunaico.com>
2025-03-12 11:56:44 -0400

    Define types for API Responses

---
+++ app/src/types.ts
@@ -0,0 +1,61 @@
+export type Person = {
+  name: string;
+  birth_year: string;
+  eye_color: string;
+  gender: string;
+  hair_color: string;
+  height: string;
+  homeworld: string;
+  species: string[];
+  starships: string[];
+  vehicles: string[];
+};
+
+export type Planet = {
+  name: string;
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+};
+
+export type Starship = {
+  name: string;
+  model: string;
+  manufacturer: string;
+  cost_in_credits: string;
+  length: string;
+  max_atmosphering_speed: string;
+  crew: string;
+};
+
+export type Vehicle = {
+  name: string;
+  model: string;
+  manufacturer: string;
+  cost_in_credits: string;
+  length: string;
+  max_atmosphering_speed: string;
+  crew: string;
+};
+
+export type Film = {
+  title: string;
+  episode_id: number;
+  opening_crawl: string;
+  director: string;
+  producer: string;
+  release_date: string;
+};
+
+export type Species = {
+  name: string;
+  classification: string;
+  designation: string;
+  average_height: string;
+  average_lifespan: string;
+  homeworld: string;
+  language: string;
+};
