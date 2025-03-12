e2633d433e6ff50b757bbbdcae75e9bb3a796bd1
benjamin-kunai <benjamin.hybart@kunaico.com>
2025-03-12 14:41:40 -0400

    Basic implementation of search functionality

--- app/src/api/client.ts
+++ app/src/api/client.ts
@@ -1,4 +1,4 @@
-import { Endpoint,  EntityType } from "../types";
+import { Endpoint,  EntityType } from "./types";
 
 const BASE_URL = 'https://swapi.dev/api';
 
@@ -14,45 +14,37 @@ export class ApiClient {
         })
         const response = await fetch(`${this.baseUrl}/${endpoint}?${params}`);
         const data = await response.json();
+        console.log(data.results);
         return data.results;
     }
 
-    public async searchPeople(query: Endpoint): Promise<EntityType['people']> {
+    public async searchPeople(query: string): Promise<EntityType['people']> {
         const data = await this.search<'people'>('people', query);
         return data;
     }
 
-    public async searchPlanets(query: Endpoint): Promise<EntityType['planets']> {
+    public async searchPlanets(query: string): Promise<EntityType['planets']> {
         const data = await this.search<'planets'>('planets', query);
         return data;
     }
 
-    public async searchVehicles(query: Endpoint): Promise<EntityType['vehicles']> {
+    public async searchVehicles(query: string): Promise<EntityType['vehicles']> {
         const data = await this.search<'vehicles'>('vehicles', query);
         return data;
     }
 
-    public async searchStarships(query: Endpoint): Promise<EntityType['starships']> {
+    public async searchStarships(query: string): Promise<EntityType['starships']> {
         const data = await this.search<'starships'>('starships', query);
         return data;
     }
 
-    public async searchFilms(query: Endpoint): Promise<EntityType['films']> {
+    public async searchFilms(query: string): Promise<EntityType['films']> {
         const data = await this.search<'films'>('films', query);
         return data;
     }
 
-    public async searchSpecies(query: Endpoint): Promise<EntityType['species']> {
+    public async searchSpecies(query: string): Promise<EntityType['species']> {
         const data = await this.search<'species'>('species', query);
         return data;
     }
-    
-    public attachEventListeners() {
-        const input = document.querySelector<HTMLInputElement>('input');
-        const button = document.querySelector<HTMLButtonElement>('button');
-        button?.addEventListener('click', async () => {
-
-        })
-    }
-    
 }
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,18 +1,66 @@
-import './style.css'
-import { ApiClient } from './api/client';
+import "./style.css";
+import { ApiClient } from "./api/client";
 
 const apiClient = new ApiClient();
 
+const attachEventListeners = () => {
+  const form = document.querySelector<HTMLFormElement>('#form');
+  
+  form?.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const searchTerm = formData.get('search') as string;
+    const searchType = formData.get('group1') as string;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+    switch (searchType) {
+      case 'people':
+        await apiClient.searchPeople(searchTerm);
+        break;
+      case 'planets':
+        await apiClient.searchPlanets(searchTerm);
+        break;
+      case 'vehicles':
+        await apiClient.searchVehicles(searchTerm);
+        break;
+      case 'starships':
+        await apiClient.searchStarships(searchTerm);
+        break;
+      case 'films':
+        await apiClient.searchFilms(searchTerm);
+        break;
+      case 'species':
+        await apiClient.searchSpecies(searchTerm);
+        break;
+    }
+  });
+  };
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>Vite/TypeScript App</h1>
-    <input type="text"/>
-    <div class="card">
-      <button  type="button">Fetch Some Data</button>
+    <form id="form">
+    <fieldset id="group1">
+          <input id="people" type="radio" value="people" name="group1" >
+          <label for="people">People</label>
+          <input id="planets" type="radio" value="planets" name="group1">
+          <label for="planets">Planets</label>
+          <input id="vehicles" type="radio" value="vehicles" name="group1">
+          <label for="vehicles">Vehicles</label>
+          <input id="starships" type="radio" value="starships" name="group1">
+          <label for="starships">Starships</label>
+          <input id="films" type="radio" value="films" name="group1">
+          <label for="films">Films</label>
+          <input id="species" type="radio" value="species" name="group1">
+          <label for="species">Species</label>
+    </fieldset>
+      <input name="search" id="search" type="text"/>
+    <div class="card" >
+      <input  type="submit" value="Submit">
     </div>
+    </form>
     <p class="read-the-docs">
     </p>
   </div>
-`
-apiClient.attachEventListeners();
+`;
+
+attachEventListeners();
